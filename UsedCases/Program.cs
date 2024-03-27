5f7b1f633f74077986e72d7fbb3db20e26a38be4
pranavhirulkar21 <pranavhirulkar21@gmail.com>
2024-03-27 22:02:51 +0530

    UC3 added

--- UsedCases/Program.cs
+++ UsedCases/Program.cs
@@ -6,6 +6,7 @@ namespace UsedCases
     {
         static void Main(string[] args)
         {
+            int IS_PART_TIME = 0;
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
 
@@ -14,12 +15,20 @@ namespace UsedCases
 
             Random ran = new Random();
 
-            int empCheck = ran.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = ran.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
-                empHrs = 8;            }
+                Console.WriteLine("Calculating Part-Time Wages : ");
+                empHrs = 4;   
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Calculating Full-Time Wages : ");
+                empHrs = 8;
+            }
             else
             {
+                Console.WriteLine("Employee is not working : ");
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
--- UsedCases/obj/Debug/netcoreapp3.1/UsedCases.AssemblyInfo.cs
+++ UsedCases/obj/Debug/netcoreapp3.1/UsedCases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsedCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c398aefe62193604e3ebf65608981a6309eea39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d7a42812b0be2490d85fe83d3f6c0b70766f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsedCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsedCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
