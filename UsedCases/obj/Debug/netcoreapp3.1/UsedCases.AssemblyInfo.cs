11d7a42812b0be2490d85fe83d3f6c0b70766f4e
pranavhirulkar21 <pranavhirulkar21@gmail.com>
2024-03-27 18:49:39 +0530

    UC2 added

--- UsedCases/obj/Debug/netcoreapp3.1/UsedCases.AssemblyInfo.cs
+++ UsedCases/obj/Debug/netcoreapp3.1/UsedCases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsedCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c398aefe62193604e3ebf65608981a6309eea39")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsedCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsedCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
