1a5a5f345309ee8f384dddecc0c03e1112bb826c
pranavhirulkar21 <pranavhirulkar21@gmail.com>
2024-03-27 18:44:11 +0530

    1st commit

---
+++ UsedCases/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsedCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random ran = new Random();
+
+            int empCheck = ran.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present!!!");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!!!");
+            }
+        }
+    }
+}
